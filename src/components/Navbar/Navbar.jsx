244d04407211a8bd6443a8e54391920605bbacdf
Christ-Phanuel DAGBO <dagbokady@gmail.com>
2025-01-03 21:08:09 +0000

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -69,7 +69,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            {*/  <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/project"
